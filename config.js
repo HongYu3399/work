4737ab7e3d8455dcc5339be9fb8439ab46a2e96e
HongYu3399 <a0912647173@gmail.com>
2025-01-29 00:23:02 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-// 使用完整的 ESM URL
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
+// 改用 CDN 引入
+const { createClient } = supabase;  // 直接從全局變量獲取
 
 const SUPABASE_URL = 'https://aoxjpnudahusexmhivjc.supabase.co'
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFveGpwbnVkYWh1c2V4bWhpdmpjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgwNzk5ODcsImV4cCI6MjA1MzY1NTk4N30.ciwFFaYj-EI2B9xUy0gGFobikehg_tocdFmrdgLGu9E'
